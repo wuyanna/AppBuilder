116104309c36ead50389cf3aa57500785eeb9a6c
Yanna Wu <wuyanna2009@gmail.com>
2013-09-07 00:22:35 -0400

    Add user & app scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 AppBuilderRuby::Application.routes.draw do
+  resources :apps
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
